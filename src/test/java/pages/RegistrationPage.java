78cb90a26786505a97d5473a5192a1b9bee4563f
Crayz2020 <andrey_zamer@mail.ru>
2022-02-14 17:35:49 +0500

    added some pageobjects

--- src/test/java/pages/RegistrationPage.java
+++ src/test/java/pages/RegistrationPage.java
@@ -6,6 +6,7 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 
 public class RegistrationPage {
+
     // locators
     SelenideElement firstnameInput = $("#firstName");
     SelenideElement lastnameInput = $("#lastName");
