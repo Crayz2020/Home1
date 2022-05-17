a8ee351f8aee14b14f3752914d67741c1167700b
Crayz2020 <andrey_zamer@mail.ru>
2022-05-17 13:43:54 +0500

    added video

--- src/test/java/test/TestBase.java
+++ src/test/java/test/TestBase.java
@@ -6,6 +6,7 @@ import io.qameta.allure.Allure;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.RegistrationPage;
 
 public class TestBase {
@@ -16,6 +17,10 @@ public class TestBase {
         SelenideLogger.addListener("allure", new AllureSelenide());
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
     }
